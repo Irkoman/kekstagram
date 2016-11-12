96ee5ad3ae29223ad11775e7442c402d474ea913
Irina Smirnova <smirnovapr@mail.ru>
2016-11-12 22:32:58 +0300

    Pagination, filters and EventListeners

--- src/js/picture.js
+++ src/js/picture.js
@@ -14,52 +14,58 @@ var gallery = require('./gallery');
 var PHOTO_LOAD_TIMEOUT = 10000;
 var template = document.getElementById('picture-template');
 var templateContainer = 'content' in template ? template.content : template;
-var templateElement = templateContainer.querySelector('.picture');
 
-var getPictureElement = function(picture) {
-  var pictureElement = templateElement.cloneNode(true);
-  pictureElement.querySelector('.picture-comments').textContent = picture.comments;
-  pictureElement.querySelector('.picture-likes').textContent = picture.likes;
-
-  /**
-   * Изображениям добавляются обработчики загрузки и ошибки
-   */
-  var photo = new Image(182, 182);
-  var photoTimeout = null;
-
-  photo.onload = function() {
-    clearTimeout(photoTimeout);
-    var img = pictureElement.querySelector('img');
-    pictureElement.replaceChild(photo, img);
-  };
-
-  photo.onerror = function() {
-    pictureElement.classList.add('picture-load-failure');
-  };
-
-  photo.src = picture.url;
-
-  photoTimeout = setTimeout(function() {
-    pictureElement.classList.add('picture-load-failure');
-  }, PHOTO_LOAD_TIMEOUT);
-
-  return pictureElement;
-};
-
-var Picture = function(data, index) {
+var Picture = function(data) {
+  var self = this;
   this.data = data;
-  this.element = getPictureElement(this.data);
+  this.element = templateContainer.querySelector('.picture').cloneNode(true);
+  this.element.querySelector('.picture-comments').textContent = data.comments;
+  this.element.querySelector('.picture-likes').textContent = data.likes;
 
   this.element.onclick = function(event) {
     event.preventDefault();
-    if(!event.target.classList.contains('picture-load-failure')) {
-      gallery.show(index);
+    var element = event.currentTarget;
+    var parent = event.currentTarget.parentNode;
+
+    self.index = Array.prototype.indexOf.call(parent.childNodes, element);
+
+    if (!event.target.classList.contains('picture-load-failure')) {
+      gallery.show(self.index - 1);
     }
   };
 
-  this.remove = function() {
-    this.element.onclick = null;
+  var img = this.element.querySelector('img');
+  this.renderPicture(img, data.url);
+};
+
+Picture.prototype.remove = function() {
+  this.element.onclick = null;
+};
+
+Picture.prototype.renderPicture = function(img, url) {
+  var self = this;
+  var photo = new Image(182, 182);
+  var photoTimeout = null;
+
+  /**
+   * Обработчики загрузки и ошибки
+   */
+  photo.onload = function() {
+    clearTimeout(photoTimeout);
+    self.element.replaceChild(photo, img);
   };
+
+  photo.src = url;
+
+  photo.onerror = function() {
+    self.element.classList.add('picture-load-failure');
+  };
+
+  photoTimeout = setTimeout(function() {
+    self.element.classList.add('picture-load-failure');
+  }, PHOTO_LOAD_TIMEOUT);
+
+  return this.element;
 };
 
 module.exports = Picture;
